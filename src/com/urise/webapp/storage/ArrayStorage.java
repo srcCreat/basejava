45cd3d8cbed1be939d34b0790d235d994295ba18
Александр <aldrpavlov0714@gmail.com>
2020-04-06 20:54:57 +1000

    Add static members

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -20,7 +21,7 @@ public class ArrayStorage {
         //TODO check to repeat
         if (getIndex(r.getUuid()) != -1) {
             System.out.println("Резюме " + r.getUuid() + " уже существует");
-        } else if (size == storage.length) {
+        } else if (size == STORAGE_LIMIT) {
             System.out.println("Место хранения переполнено!");
         } else {
             storage[size] = r;
